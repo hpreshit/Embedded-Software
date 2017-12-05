3f676a12ba2c9187e3f6fe74d0e93475e1ea6f89
shivam3k <31378206+shivam3k@users.noreply.github.com>
2017-12-05 04:51:01 -0700

    Adding uncommented header files

---
+++ project3/include/common/conversion.h
@@ -0,0 +1,16 @@
+/*
+ * conversion.h
+ *
+ *  Created on: 03-Dec-2017
+ *      Author: defaultuser0
+ */
+
+#ifndef SOURCES_CONVERSION_H_
+#define SOURCES_CONVERSION_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+uint8_t my_itoa(int32_t data,uint8_t* ptr, uint32_t base);
+
+#endif /* SOURCES_CONVERSION_H_ */
